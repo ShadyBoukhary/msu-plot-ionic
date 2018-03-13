b2ade21e79e4029765855f2e2b3095b1a7e1094c
Shady Boukhary <32588376+ShadyBoukhary@users.noreply.github.com>
2018-03-13 00:47:23 -0500

    Added alerts, no notifications yet

---
+++ src/firebase-config/app.firebase.config.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyBlM7GbV_fx5JTwC09xSaM-GmUQzQlaSC0",
+    authDomain: "msu-plot.firebaseapp.com",
+    databaseURL: "https://msu-plot.firebaseio.com",
+    projectId: "msu-plot",
+    storageBucket: "",
+    messagingSenderId: "271158989541"
+};
